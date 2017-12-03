c1056252d0639d2bb12fdcac5c0098d8cce2e228
suoyiman <suoyiman@163.com>
2017-12-03 21:28:29 +0800

    tf_session

---
+++ tf_session.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+matrix1 = tf.constant([[3, 3]])
+matrix2 = tf.constant([[2],
+                       [2]])
+product = tf.matmul(matrix1,matrix2)
+print(product)
+session = tf.Session()
+result = session.run(product)
+print(result)
